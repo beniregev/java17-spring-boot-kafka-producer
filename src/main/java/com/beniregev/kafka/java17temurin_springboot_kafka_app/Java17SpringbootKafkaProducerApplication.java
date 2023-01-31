70cac9e104a66b0e4f76e81bb1315dc59f2cc474
binyamin.regev <beniregev@gmail.com>
2023-02-01 00:35:17 +0200

    Adding Application and Test-Application classes

---
+++ src/main/java/com/beniregev/kafka/java17temurin_springboot_kafka_app/Java17SpringbootKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.beniregev.kafka.java17temurin_springboot_kafka_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17SpringbootKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java17SpringbootKafkaProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beniregev/kafka/java17temurin_springboot_kafka_app/Java17SpringbootKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beniregev.kafka.java17temurin_springboot_kafka_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17SpringbootKafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
