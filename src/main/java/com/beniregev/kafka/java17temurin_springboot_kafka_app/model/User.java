f0a7d632553e54eaa6569af254c4746be8f09052
binyamin.regev <beniregev@gmail.com>
2023-02-01 00:36:23 +0200

    Adding User class for JSON serialization

---
+++ src/main/java/com/beniregev/kafka/java17temurin_springboot_kafka_app/model/User.java
@@ -0,0 +1,37 @@
+package com.beniregev.kafka.java17temurin_springboot_kafka_app.model;
+
+public class User {
+    private String name;
+    private String dept;
+    private Integer salary;
+
+    public User(String name, String dept, Integer salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
